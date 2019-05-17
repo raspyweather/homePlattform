87e0827ceb681795e71d4eb1cc3caaefde221c61
raspyweather <raspyweather@gmail.com>
2019-05-18 01:26:11 +0200

    fixed import

--- homePlattform/src/app/app-routing.module.ts
+++ homePlattform/src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ScanReceiptComponent } from './scan-receipt/scan-receipt.component';
+import { InventoryListComponent } from './inventory-list/inventory-list.component';
 
 const routes: Routes = [{
   component: ScanReceiptComponent,
-  path: "scanner"
+  path: 'scanner'
 },
 {
   component: InventoryListComponent,
-  path: "inventory"
+  path: 'inventory'
 }];
 
 @NgModule({
