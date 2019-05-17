9ab609c043dac4eb0ea3fd609617f789db35f95e
raspyweather <raspyweather@gmail.com>
2019-05-18 01:28:02 +0200

    types fixed for scan-receipt

--- homePlattform/src/app/scan-receipt/scan-receipt.component.ts
+++ homePlattform/src/app/scan-receipt/scan-receipt.component.ts
@@ -13,24 +13,24 @@ export class ScanReceiptComponent implements OnInit {
 
   ngOnInit() {
     // Grab elements, create settings, etc.
-    const camera_preview = document.getElementById('camera_preview');
-    const canvas = document.getElementById('camera_preview_canvas');
-    this.video = document.getElementById('camera');
+    const camera_preview = document.getElementById('camera_preview') as HTMLDivElement;
+    const canvas = document.getElementById('camera_preview_canvas') as HTMLCanvasElement;
+    this.video = document.getElementById('camera') as HTMLVideoElement;
 
     // Put video listeners into place
-    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({ video: true }).then((stream)=> {
-          //video.src = window.URL.createObjectURL(stream);
-          this.video.srcObject = stream;
-          this.video.play();
-        });
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+        //video.src = window.URL.createObjectURL(stream);
+        this.video.srcObject = stream;
+        this.video.play();
+      });
     }
 
     // Trigger photo take
-    document.getElementById('camera_shutter_button').addEventListener('click', ()=> {
+    document.getElementById('camera_shutter_button').addEventListener('click', () => {
       canvas.getContext('2d').drawImage(this.video, 0, 0, canvas.width, canvas.height);
-      camera_preview.style.backgroundImage = "url('"+canvas.toDataURL('image/jpeg')+"')";
-      document.getElementById("loading_overlay").classList.add("active")
+      camera_preview.style.backgroundImage = 'url(\'' + canvas.toDataURL('image/jpeg') + '\')';
+      document.getElementById('loading_overlay').classList.add('active')
     });
   }
 
