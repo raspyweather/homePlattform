362bcec18e8463415f717ee3c7fc95ccff1ab057
AlexanderMelde <2115644+AlexanderMelde@users.noreply.github.com>
2019-05-18 01:13:05 +0200

    added scanner

--- homePlattform/src/app/app-routing.module.ts
+++ homePlattform/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ScanReceiptComponent } from './scan-receipt/scan-receipt.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  component: ScanReceiptComponent,
+  path: "scanner"
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
