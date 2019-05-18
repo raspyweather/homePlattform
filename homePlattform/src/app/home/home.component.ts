4b84f310dc6441c2c025c143fa8d9bcff0e07268
Christian Wernet <christian.wernet@cern.ch>
2019-05-18 15:25:46 +0200

    Add carousel to homepage

--- homePlattform/src/app/home/home.component.ts
+++ homePlattform/src/app/home/home.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  images = ['https://www.lidl.de/de/asset/images/1918_Sixt_HPFB_Desktop_1182x300px.jpg', 'https://www.lidl.de/de/asset/images/1920_sis_Dessous_HPFB_Desktop_1182x300px.jpg', 'https://www.lidl.de/de/asset/images/1921_Mo_Mode_Theme_Hero.jpg'];
+
   constructor() { }
 
   ngOnInit() {
