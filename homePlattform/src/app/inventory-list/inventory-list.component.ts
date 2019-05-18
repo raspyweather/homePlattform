bc87298160541d1981740cace6445c190ee2f2d4
raspyweather <raspyweather@gmail.com>
2019-05-18 10:29:53 +0200

    ng lint fixes

--- homePlattform/src/app/inventory-list/inventory-list.component.ts
+++ homePlattform/src/app/inventory-list/inventory-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { resetCompiledComponents } from '@angular/core/src/render3/jit/module';
 import { inventoryItem } from './inventoryItem.interface';
 import { ActivatedRoute } from '@angular/router';
-import * as moment from "moment";
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-inventory-list',
@@ -14,7 +14,7 @@ export class InventoryListComponent implements OnInit {
   public inventoryElements: inventoryItem[] = [];
   public formProductName = '';
   public formProductPrice = 0.00;
-  public formProductBestBefore = moment().locale("de").locale("de").format("DD-MM-YYYY");
+  public formProductBestBefore = moment().locale('de').locale('de').format('DD-MM-YYYY');
 
   constructor(private readonly route: ActivatedRoute) { }
 
@@ -23,40 +23,47 @@ export class InventoryListComponent implements OnInit {
     if (localStorage.getItem('inventoryList') !== null) {
       inventoryListJsonString = localStorage.getItem('inventoryList');
       const inventoryListObject = JSON.parse(inventoryListJsonString);
-      this.inventoryElements.unshift(...inventoryListObject['inventory']);
+      this.inventoryElements.unshift(...inventoryListObject.inventory);
     }
-    this.inventoryElements.unshift({ name: "Apple Loopies", price: 1.99, bestBefore: "30-07-2020", addedAt: "18. Mai 2019 um 08:00 Uhr" })
+    this.inventoryElements.unshift({ name: 'Apple Loopies', price: 1.99, bestBefore: '30-07-2020', addedAt: '18. Mai 2019 um 08:00 Uhr' });
     this.route.params.subscribe((param) => {
       if ('fromScan' in param) {
-        this.addReceiptItems()
+        this.addReceiptItems();
       }
-    })
-    //document.getElementById("inventoryList").classList.add("useAnimations");
+    });
+    // document.getElementById("inventoryList").classList.add("useAnimations");
   }
 
   public addReceiptItems() {
-    this.inventoryElements.unshift(...[{ name: 'Müllermilch', price: 0.59, bestBefore: '06-06-19', addedAt: moment().locale("de").format('Do MMMM YYYY [um] hh:mm [Uhr]') },
-    { name: 'Tortilla Chips', price: 0.89, bestBefore: '10-01-20', addedAt: moment().locale("de").format('Do MMMM YYYY [um] hh:mm [Uhr]') },
-    { name: 'Salzstangen JA!', price: 0.39, bestBefore: '10-08-19', addedAt: moment().locale("de").format('Do MMMM YYYY [um] hh:mm [Uhr]') },
-    { name: 'Pepper Cola', price: 1.19, bestBefore: '02-12-19', addedAt: moment().locale("de").format('Do MMMM YYYY [um] hh:mm [Uhr]') },
-    { name: 'Vio Limo Zitrone', price: 1.49, bestBefore: '30-09-19', addedAt: moment().locale("de").format('Do MMMM YYYY [um] hh:mm [Uhr]') },
-    { name: 'Pfanner pure tea', price: 0.99, bestBefore: '11-04-20', addedAt: moment().locale("de").format('Do MMMM YYYY [um] hh:mm [Uhr]') }]);
+    this.inventoryElements.unshift(...[
+      { name: 'Müllermilch', price: 0.59, bestBefore: '06-06-19', addedAt: moment().locale('de').format('Do MMMM YYYY [um] hh:mm [Uhr]') },
+      { name: 'Tortilla Chips', price: 0.89, bestBefore: '10-01-20', addedAt: moment().locale('de').format('Do MMMM YYYY [um] hh:mm [Uhr]') },
+      { name: 'Salzstangen JA!', price: 0.39, bestBefore: '10-08-19', addedAt: moment().locale('de').format('Do MMMM YYYY [um] hh:mm [Uhr]') },
+      { name: 'Pepper Cola', price: 1.19, bestBefore: '02-12-19', addedAt: moment().locale('de').format('Do MMMM YYYY [um] hh:mm [Uhr]') },
+      { name: 'Vio Limo Zitrone', price: 1.49, bestBefore: '30-09-19', addedAt: moment().locale('de').format('Do MMMM YYYY [um] hh:mm [Uhr]') },
+      { name: 'Pfanner pure tea', price: 0.99, bestBefore: '11-04-20', addedAt: moment().locale('de').format('Do MMMM YYYY [um] hh:mm [Uhr]') }]);
   }
 
   addReceiptItemManually() {
-    const inventoryItem: inventoryItem = { name: this.formProductName, price: this.formProductPrice, bestBefore: this.formProductBestBefore, addedAt: moment().locale("de").format('Do MMMM YYYY [um] hh:mm [Uhr]') };
+    const inventoryItem: inventoryItem =
+    {
+      name: this.formProductName,
+      price: this.formProductPrice,
+      bestBefore: this.formProductBestBefore,
+      addedAt: moment().locale('de').format('Do MMMM YYYY [um] hh:mm [Uhr]')
+    };
     this.inventoryElements.unshift(inventoryItem);
     this.appendInventoryItemToLocalStorage(inventoryItem);
     this.resetFormFields();
   }
 
   removeInventoryItem(idx: number) {
-    let inventoryItemToBeRemoved = this.inventoryElements.filter((x, i) => i === idx);
+    const inventoryItemToBeRemoved = this.inventoryElements.filter((x, i) => i === idx);
     this.removeInventoryItemFromLocalStorage(inventoryItemToBeRemoved[0]);
     this.inventoryElements = this.inventoryElements.filter((x, i) => i !== idx);
   }
 
-  removeInventoryItemFromLocalStorage(inventoryItem: inventoryItem){
+  removeInventoryItemFromLocalStorage(inventoryItem: inventoryItem) {
     let inventoryListJsonString = '';
     let inventoryListObject = { inventory: [] };
 
--- homePlattform/src/app/scan-receipt/scan-receipt.component.ts
+++ homePlattform/src/app/scan-receipt/scan-receipt.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { InventoryListComponent } from '../inventory-list/inventory-list.component';
 
@@ -7,24 +7,24 @@ import { InventoryListComponent } from '../inventory-list/inventory-list.compone
   templateUrl: './scan-receipt.component.html',
   styleUrls: ['./scan-receipt.component.scss']
 })
-export class ScanReceiptComponent implements OnInit {
+export class ScanReceiptComponent implements OnInit, OnDestroy {
 
   video;
 
-  constructor(private readonly router:Router) {
-  
+  constructor(private readonly router: Router) {
+
   }
 
   ngOnInit() {
     // Grab elements, create settings, etc.
-    const camera_preview = document.getElementById('camera_preview') as HTMLDivElement;
+    const cameraPreview = document.getElementById('camera_preview') as HTMLDivElement;
     const canvas = document.getElementById('camera_preview_canvas') as HTMLCanvasElement;
     this.video = document.getElementById('camera') as HTMLVideoElement;
 
     // Put video listeners into place
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
       navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
-        //video.src = window.URL.createObjectURL(stream);
+        // video.src = window.URL.createObjectURL(stream);
         this.video.srcObject = stream;
         this.video.play();
       });
@@ -33,16 +33,16 @@ export class ScanReceiptComponent implements OnInit {
     // Trigger photo take
     document.getElementById('camera_shutter_button').addEventListener('click', () => {
       canvas.getContext('2d').drawImage(this.video, 0, 0, canvas.width, canvas.height);
-      camera_preview.style.backgroundImage = 'url(\'' + canvas.toDataURL('image/jpeg') + '\')';
-      document.getElementById('loading_overlay').classList.add('active')
-      window.setTimeout(()=>{
-        this.router.navigateByUrl("/inventory/:fromScan")
-      },500)
+      cameraPreview.style.backgroundImage = 'url(\'' + canvas.toDataURL('image/jpeg') + '\')';
+      document.getElementById('loading_overlay').classList.add('active');
+      window.setTimeout(() => {
+        this.router.navigateByUrl('/inventory/:fromScan');
+      }, 500);
     });
   }
 
   ngOnDestroy() {
-    this.video.pause()
+    this.video.pause();
   }
 
 }
