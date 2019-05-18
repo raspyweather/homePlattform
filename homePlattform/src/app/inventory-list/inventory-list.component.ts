8f47ba5855a810ea087a1697e02af955908cbf9d
Christian Wernet <christian.wernet@cern.ch>
2019-05-18 09:41:07 +0200

    Ability to remove items from inventory

--- homePlattform/src/app/inventory-list/inventory-list.component.ts
+++ homePlattform/src/app/inventory-list/inventory-list.component.ts
@@ -16,17 +16,17 @@ export class InventoryListComponent implements OnInit {
   public formProductPrice = 0.00;
   public formProductBestBefore = moment().locale("de").locale("de").format("DD-MM-YYYY");
 
-  constructor(private readonly route:ActivatedRoute) { }
+  constructor(private readonly route: ActivatedRoute) { }
 
   ngOnInit() {
     let inventoryListJsonString = '';
-    if (localStorage.getItem('inventoryList') != null) {
+    if (localStorage.getItem('inventoryList') !== null) {
       inventoryListJsonString = localStorage.getItem('inventoryList');
       const inventoryListObject = JSON.parse(inventoryListJsonString);
       this.inventoryElements.push(...inventoryListObject['inventory']);
     }
-    this.inventoryElements.push({name: "Apple Loopies", price: 1.99, bestBefore: "30-07-2020", addedAt: "18. Mai 2019 um 08:00 Uhr"})
-    this.route.params.subscribe((param)=>{
+    this.inventoryElements.push({ name: "Apple Loopies", price: 1.99, bestBefore: "30-07-2020", addedAt: "18. Mai 2019 um 08:00 Uhr" })
+    this.route.params.subscribe((param) => {
       if ('fromScan' in param) {
         this.addReceiptItems()
       }
@@ -35,11 +35,11 @@ export class InventoryListComponent implements OnInit {
 
   public addReceiptItems() {
     this.inventoryElements.push(...[{ name: 'Müllermilch', price: 0.59, bestBefore: '06-06-19', addedAt: moment().locale("de").format('Do MMMM YYYY [um] hh:mm [Uhr]') },
-    { name: 'Tortilla Chips', price: 0.89, bestBefore: '10-01-20', addedAt: moment().locale("de").format('Do MMMM YYYY [um] hh:mm [Uhr]')  },
-    { name: 'Salzstangen JA!', price: 0.39, bestBefore: '10-08-19', addedAt: moment().locale("de").format('Do MMMM YYYY [um] hh:mm [Uhr]')  },
-    { name: 'Pepper Cola', price: 1.19, bestBefore: '02-12-19', addedAt: moment().locale("de").format('Do MMMM YYYY [um] hh:mm [Uhr]')  },
-    { name: 'Vio Limo Zitrone', price: 1.49, bestBefore: '30-09-19', addedAt: moment().locale("de").format('Do MMMM YYYY [um] hh:mm [Uhr]')  },
-    { name: 'Pfanner pure tea', price: 0.99, bestBefore: '11-04-20', addedAt: moment().locale("de").format('Do MMMM YYYY [um] hh:mm [Uhr]')  }]);
+    { name: 'Tortilla Chips', price: 0.89, bestBefore: '10-01-20', addedAt: moment().locale("de").format('Do MMMM YYYY [um] hh:mm [Uhr]') },
+    { name: 'Salzstangen JA!', price: 0.39, bestBefore: '10-08-19', addedAt: moment().locale("de").format('Do MMMM YYYY [um] hh:mm [Uhr]') },
+    { name: 'Pepper Cola', price: 1.19, bestBefore: '02-12-19', addedAt: moment().locale("de").format('Do MMMM YYYY [um] hh:mm [Uhr]') },
+    { name: 'Vio Limo Zitrone', price: 1.49, bestBefore: '30-09-19', addedAt: moment().locale("de").format('Do MMMM YYYY [um] hh:mm [Uhr]') },
+    { name: 'Pfanner pure tea', price: 0.99, bestBefore: '11-04-20', addedAt: moment().locale("de").format('Do MMMM YYYY [um] hh:mm [Uhr]') }]);
   }
 
   addReceiptItemManually() {
@@ -49,11 +49,15 @@ export class InventoryListComponent implements OnInit {
     this.resetFormFields();
   }
 
+  removeInventoryItem(idx: number) {
+    this.inventoryElements = this.inventoryElements.filter((x, i) => i !== idx);
+  }
+
   appendInventoryItemToLocalStorage(inventoryItem: inventoryItem) {
     let inventoryListJsonString = '';
     let inventoryListObject = { inventory: [] };
 
-    if (localStorage.getItem('inventoryList') != null) {
+    if (localStorage.getItem('inventoryList') !== null) {
       inventoryListJsonString = localStorage.getItem('inventoryList');
       inventoryListObject = JSON.parse(inventoryListJsonString);
     } else {
