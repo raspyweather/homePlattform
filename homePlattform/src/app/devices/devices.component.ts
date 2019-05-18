21689505aa007357cfc66a99449c77fb1a6002a4
raspyweather <raspyweather@gmail.com>
2019-05-19 00:28:27 +0200

    added information for default devices

--- homePlattform/src/app/devices/devices.component.ts
+++ homePlattform/src/app/devices/devices.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { smartDevice, SmartDeviceType } from './smartDevice.interface';
+import { SmartDevice, SmartDeviceType } from './smartDevice.interface';
 import { DeviceService } from '../device.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-devices',
@@ -10,7 +11,7 @@ import { DeviceService } from '../device.service';
 export class DevicesComponent implements OnInit {
 
 
-  public registeredDevices: smartDevice[] = [];
+  public registeredDevices: SmartDevice[] = [];
   public formDeviceName = '';
   public formDeviceAssociation = '';
   public showForm = false;
@@ -18,22 +19,23 @@ export class DevicesComponent implements OnInit {
   constructor(private readonly deviceService: DeviceService) { }
 
   ngOnInit() {
-    const deviceListJsonString = '';
-    const devices = this.deviceService.getDevices();
-    if (devices.length === 0) {
+    this.registeredDevices = this.deviceService.getDevices();
+    if (this.registeredDevices.length === 0) {
       this.insertDevice(
         {
           name: 'Mixer',
           associatedDevice: 'Mixer',
           currentEnergyConsumption: 0.2,
-          type: SmartDeviceType.SmartPlug
+          type: SmartDeviceType.SmartPlug,
+          addedAt: moment('20150420', 'YYYYMMDD').locale('DE').format()
         });
       this.insertDevice(
         {
           name: '60" Plasma TV',
           associatedDevice: 'Plasma TV',
           currentEnergyConsumption: 450.3,
-          type: SmartDeviceType.SmartPlug
+          type: SmartDeviceType.SmartPlug,
+          addedAt: moment('20120620', 'YYYYMMDD').locale('DE').format()
         });
     }
   }
@@ -45,17 +47,18 @@ export class DevicesComponent implements OnInit {
     this.deviceService.setDevices(elements.filter((x, i) => i !== deviceIndex));
   }
 
-  insertDevice(device: smartDevice) {
+  insertDevice(device: SmartDevice) {
     this.registeredDevices.push(device);
     this.deviceService.setDevices(this.registeredDevices);
   }
 
   registerDeviceManually() {
-    const device: smartDevice = {
+    const device: SmartDevice = {
       name: this.formDeviceName,
       associatedDevice: this.formDeviceAssociation,
       currentEnergyConsumption: parseFloat(((Math.random() * (900.00 - 0.00)) / 4.0).toFixed(2)),
-      type: SmartDeviceType.SmartPlug
+      type: SmartDeviceType.SmartPlug,
+      addedAt: moment().locale('DE').format()
     };
     this.insertDevice(device);
     this.resetFormFields();
