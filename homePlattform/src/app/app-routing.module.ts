24dc2ad675e151d86e8db9f39ebdf38e451152a9
Christian Wernet <christian.wernet@cern.ch>
2019-05-18 02:38:13 +0200

    Add home component containing scan button

--- homePlattform/src/app/app-routing.module.ts
+++ homePlattform/src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { InventoryListComponent } from './inventory-list/inventory-list.component';
 import { ScanReceiptComponent } from './scan-receipt/scan-receipt.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [{
   component: ScanReceiptComponent,
   path: 'scanner'
+},
+{
+  component: HomeComponent,
+  path: ''
 },
 {
   component: InventoryListComponent,
--- homePlattform/src/app/app.module.ts
+++ homePlattform/src/app/app.module.ts
@@ -7,14 +7,16 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './navbar/navbar.component';
 import { InventoryListComponent } from './inventory-list/inventory-list.component';
 import { ScanReceiptComponent } from './scan-receipt/scan-receipt.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
     AppComponent,
+    ScanReceiptComponent,
     InventoryListComponent,
-    ScanReceiptComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
